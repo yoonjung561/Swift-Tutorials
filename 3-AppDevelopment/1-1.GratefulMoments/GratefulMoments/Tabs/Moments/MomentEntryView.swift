da2912ea6949d6c177cceccebc03e5be115905ea
yoonjung561 <kyoonjung0601@gmail.com>
2026-04-19 14:27:13 +0900

    AppDevelopment ch1-1

---
+++ 3-AppDevelopment/1-1.GratefulMoments/GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -0,0 +1,115 @@
+//
+//  MomentEntryView.swift
+//  GratefulMoments
+//
+//  Created by YoonJung Kwak on 4/19/26.
+//
+
+import SwiftUI
+import PhotosUI
+import SwiftData
+
+struct MomentEntryView: View {
+    @State private var title = ""
+    @State private var note = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
+    @State private var isShowingConcelConfirmation = false
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(DataContainer.self) private var dataContainer
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                contentStack
+            }
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Grateful For")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel", systemImage: "xmark") {
+                        if title.isEmpty, note.isEmpty, imageData == nil {
+                            dismiss()
+                        }
+                        else {
+                            isShowingConcelConfirmation = true
+                        }
+                    }
+                    .confirmationDialog("Discard Moment", isPresented: $isShowingConcelConfirmation) {
+                        Button("Discard Moment", role: .destructive) {
+                            dismiss()
+                        }
+                    }
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Add", systemImage: "checkmark") {
+                        let newMoment = Moment(
+                            title: title,
+                            note: note,
+                            imageData: imageData,
+                            timestamp: .now
+                        )
+                        dataContainer.context.insert(newMoment)
+                        do {
+                            try dataContainer.context.save()
+                            dismiss()
+                        } catch {
+                            // Don't dismiss
+                        }
+                    }
+                    .disabled(title.isEmpty)
+                }
+            }
+        }
+    }
+    
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
+        }
+    }
+    
+    var contentStack: some View {
+        VStack(alignment: .leading) {
+            TextField(text: $title) {
+                Text("Title (Required)")
+            }
+            .font(.title.bold())
+            .padding(.top, 48)
+            Divider()
+            
+            TextField("Log your small wins", text: $note, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .lineLimit(5...Int.max)
+            
+            photoPicker
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MomentEntryView()
+        .sampleDataContainer()
+}
---
+++ 3-AppDevelopment/1-1.GratefulMoments/readme.md
@@ -0,0 +1,13 @@
+## App development _ [ch.1-1 Collect, model, and store data](https://developer.apple.com/tutorials/develop-in-swift/collect-model-and-store-data)
+
+- 
+```swift
+
+```
+
+
+## Preview
+
+![preview](./GratefulMoments-preview.png)
+
+
